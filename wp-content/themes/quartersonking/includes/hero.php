64dc6b8be44867da9e55614e988db8edb5e96be4
John Pinkerton <jpinkerton88@gmail.com>
2015-03-16 19:58:05 -0400

    use title for header text if no header text var

--- wp-content/themes/quartersonking/includes/hero.php
+++ wp-content/themes/quartersonking/includes/hero.php
@@ -1,12 +1,21 @@
-<?php if (has_post_thumbnail( $post->ID ) ): ?>
-  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-        $image = $image[0]; ?>
-<?php else :
-  $image = get_template_directory_uri() . '/img/hero-img.jpg'; ?>
-<?php endif; ?>
+<?php
+
+if (has_post_thumbnail( $post->ID ) ) {
+  $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+  $image = $image[0];
+}
+else {
+  $image = get_template_directory_uri() . '/img/hero-img.jpg';
+  $text = get_post_meta($post->ID, 'header_text', true);
+  if ($text == '') {
+    $text = get_the_title($post);
+  }
+}
+
+?>
 
 <div class="hero" style="background-image: url('<?php echo $image; ?>')">
   <div class="container">
-    <h1><?php echo get_post_meta(get_the_ID(), 'header_text', true); ?></h1>
+    <h1><?php echo $text ?></h1>
   </div>
 </div>
