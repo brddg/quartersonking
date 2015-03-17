4b97d664644547bc736f68e475e21be80489acf8
John Pinkerton <jpinkerton88@gmail.com>
2015-03-16 20:44:23 -0400

    switch to custom field for hero iamge

--- wp-content/themes/quartersonking/includes/hero.php
+++ wp-content/themes/quartersonking/includes/hero.php
@@ -1,21 +1,18 @@
 <?php
 
-if (has_post_thumbnail( $post->ID ) ) {
-  $header_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-  $header_image = $header_image[0];
-}
-else {
-  $header_image = get_template_directory_uri() . '/img/hero-img.jpg';
+$hero_image = get_field('hero_image');
+if (empty($hero_image)) {
+  $hero_image = get_template_directory_uri() . '/img/hero-img.jpg';
 }
 
-$header_text = get_field('hero_text');
-if (empty($header_text)) {
-  $header_text = get_the_title($post);
+$hero_text = get_field('hero_text');
+if (empty($hero_text)) {
+  $hero_text = get_the_title($post);
 }
 ?>
 
-<div class="hero" style="background-image: url('<?php echo $header_image; ?>')">
+<div class="hero" style="background-image: url('<?php echo $hero_image; ?>')">
   <div class="container">
-    <h1><?php echo $header_text ?></h1>
+    <h1><?php echo $hero_text; ?></h1>
   </div>
 </div>
