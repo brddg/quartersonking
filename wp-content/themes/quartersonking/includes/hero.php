0f8d482f250a7ef53bfa9a5ec837c16a61d4cad9
John Pinkerton <jpinkerton88@gmail.com>
2015-03-16 20:12:57 -0400

    fix header text issue

--- wp-content/themes/quartersonking/includes/hero.php
+++ wp-content/themes/quartersonking/includes/hero.php
@@ -1,21 +1,21 @@
 <?php
 
 if (has_post_thumbnail( $post->ID ) ) {
-  $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-  $image = $image[0];
+  $header_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+  $header_image = $header_image[0];
 }
 else {
-  $image = get_template_directory_uri() . '/img/hero-img.jpg';
-  $text = get_post_meta($post->ID, 'header_text', true);
-  if ($text == '') {
-    $text = get_the_title($post);
-  }
+  $header_image = get_template_directory_uri() . '/img/hero-img.jpg';
 }
 
+$header_text = get_post_meta($post->ID, 'header_text', true);
+if (empty($header_text)) {
+  $header_text = get_the_title($post);
+}
 ?>
 
-<div class="hero" style="background-image: url('<?php echo $image; ?>')">
+<div class="hero" style="background-image: url('<?php echo $header_image; ?>')">
   <div class="container">
-    <h1><?php echo $text ?></h1>
+    <h1><?php echo $header_text ?></h1>
   </div>
 </div>
