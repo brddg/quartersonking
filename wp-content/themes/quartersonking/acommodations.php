93d73334b71b6857a1817de653f03c21a8cd9520
John Pinkerton <jpinkerton88@gmail.com>
2015-03-20 15:43:21 -0400

    fix issue with multiple carousels

--- wp-content/themes/quartersonking/acommodations.php
+++ wp-content/themes/quartersonking/acommodations.php
@@ -46,7 +46,7 @@
                           <?php if ($more_images): ?>
 
 
-                            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                            <div id="carousel-<?php echo $i; ?>" class="carousel slide" data-ride="carousel">
                               <!-- Indicators -->
                               <ol class="carousel-indicators">
                                 <?php for ($ind = 0; $ind < count($more_images); $ind++): ?>
@@ -64,11 +64,11 @@
                               </div>
 
                               <!-- Controls -->
-                              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                              <a class="left carousel-control" href="#carousel-<?php echo $i; ?>" role="button" data-slide="prev">
                                 <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                                 <span class="sr-only">Previous</span>
                               </a>
-                              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                              <a class="right carousel-control" href="#carousel-<?php echo $i; ?>" role="button" data-slide="next">
                                 <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                                 <span class="sr-only">Next</span>
                               </a>
