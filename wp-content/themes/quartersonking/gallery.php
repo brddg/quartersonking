e09b0d05c4c721a931a8b834298101ed4352cf66
John Pinkerton <jpinkerton88@gmail.com>
2015-04-16 19:23:04 -0400

    add gallery page

---
+++ wp-content/themes/quartersonking/gallery.php
@@ -0,0 +1,48 @@
+<?php /* Template Name: Gallery */ get_header(); ?>
+
+  <main role="main">
+    <!-- section -->
+    <section>
+
+
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+      <?php require_once 'includes/hero.php'; ?>
+
+      <!-- article -->
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <?php the_content(); ?>
+            </div>
+          </div>
+
+          <?php edit_post_link(); ?>
+
+        </div>
+
+      </article>
+      <!-- /article -->
+
+    <?php endwhile; ?>
+
+    <?php else: ?>
+
+      <!-- article -->
+      <article>
+
+        <div class="container">
+          <h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+        </div>
+
+      </article>
+      <!-- /article -->
+
+    <?php endif; ?>
+
+    </section>
+    <!-- /section -->
+  </main>
+
+<?php get_footer(); ?>
